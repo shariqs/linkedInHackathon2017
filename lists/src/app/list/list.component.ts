4b5b71dc9b36e107a94d032d0f3c84dc8aa60185
JakeMotta <motta_jake@yahoo.com>
2017-07-15 06:27:41 -0700

    ui changes

--- lists/src/app/list/list.component.ts
+++ lists/src/app/list/list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../Services/data.service';
+import { StateService } from '../Services/state.service';
+
 
 @Component({
   selector: 'app-list',
@@ -11,7 +13,7 @@ export class ListComponent {
   shouldRenameList : boolean = false;
   list_name_form : string = 'Enter List Name!';
 
-  constructor(public dataService : DataService ) { }
+  constructor(public dataService : DataService, public stateService : StateService ) { }
 
   private newListButtonClicked(){
     this.shouldRenameList = true;
